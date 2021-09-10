3ec30cd4671894c47e2536f7a620fc25c88c24fc
Gernot Glawe <gglawe@tecracer.de>
2021-09-10 19:07:38 +0200

    S3 Listbuckets (something to begin with)

---
+++ aws-go-sdk-v2/s3/listbuckets/lister.go
@@ -0,0 +1,34 @@
+package listbuckets
+
+import (
+	"context"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+)
+
+var client *s3.Client
+
+func init(){
+	cfg, err := config.LoadDefaultConfig(context.TODO())
+	if err != nil {
+		panic("configuration error, " + err.Error())
+	}
+	client = s3.NewFromConfig(cfg)
+}
+
+func ListBuckets() ([]*string, error){
+	bucketarray := make( []*string, 0,10)
+	
+	res, err := client.ListBuckets(context.TODO(), &s3.ListBucketsInput{})
+	// Let caller handle the error
+	if err != nil {
+		return nil, err
+	}
+
+	for _, bucket := range res.Buckets {
+		bucketarray = append(bucketarray, bucket.Name)
+	}
+
+	return bucketarray, nil
+
+}
