ca0e1314366c967bbe19eb01f5eff1695e242f4c
Gernot Glawe <gglawe@tecracer.de>
2021-10-27 11:24:06 +0200

    Tasks update

--- lambda-go/lambda-cdk/hello-world/app/main.go
+++ lambda-go/lambda-cdk/hello-world/app/main.go
@@ -11,7 +11,7 @@ type MyEvent struct {
 }
 
 func HandleRequest(ctx context.Context, name MyEvent) (string, error) {
-        return fmt.Sprintf("Hello %s!", name.Name ), nil
+        return fmt.Sprintf("Hiho %s!", name.Name ), nil
 }
 
 func main() {
