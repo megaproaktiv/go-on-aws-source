c51b2ae6b3e05e4c716f1e6af6a8e613d277604b
Gernot Glawe <gglawe@tecracer.de>
2021-12-12 17:36:03 +0100

    Infrastructure Test TO

--- architectures/serverless/infra/dsl_test.go
+++ architectures/serverless/infra/dsl_test.go
@@ -1,26 +1,28 @@
 package dsl_test
 
-// import (
-// 	"testing"
+import (
+	"testing"
+	"github.com/aws/aws-sdk-go-v2/aws"
 
-// 	"github.com/aws/aws-cdk-go/awscdk/v2"
-// 	assertions "github.com/aws/aws-cdk-go/awscdk/v2/assertions"
-// 	"github.com/aws/jsii-runtime-go"
-// )
+	"github.com/aws/aws-cdk-go/awscdk/v2"
+	assertions "github.com/aws/aws-cdk-go/awscdk/v2/assertions"
+	
+	"dsl"
+)
 
 // example tests. To run these tests, uncomment this file along with the
 // example resource in dsl_test.go
-// func TestDslStack(t *testing.T) {
-// 	// GIVEN
-// 	app := awscdk.NewApp(nil)
+func TestDslStack(t *testing.T) {
+	// GIVEN
+	app := awscdk.NewApp(nil)
 
-// 	// WHEN
-// 	stack := NewDslStack(app, "MyStack", nil)
+	// WHEN
+	stack := dsl.NewDslStack(app, "MyStack", nil)
 
-// 	// THEN
-// 	template := assertions.Template_FromStack(stack)
+	// THEN
+	template := assertions.Template_FromStack(stack)
 
-// 	template.HasResourceProperties(jsii.String("AWS::SQS::Queue"), map[string]interface{}{
-// 		"VisibilityTimeout": 300,
-// 	})
-// }
+	template.HasResourceProperties(aws.String("AWS::Lambda::Function"), map[string]interface{}{
+		"Runtime": "go1.x",
+	})
+}
