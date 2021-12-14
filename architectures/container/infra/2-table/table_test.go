5843682fbf47484816886d707ff8bbdc65c87198
Gernot Glawe <gglawe@tecracer.de>
2021-12-14 19:49:39 +0100

    Table Test fail

---
+++ architectures/container/infra/2-table/table_test.go
@@ -0,0 +1,27 @@
+package table_test
+
+
+import (
+	"testing"
+	"github.com/aws/aws-sdk-go-v2/aws"
+
+	"github.com/aws/aws-cdk-go/awscdk/v2"
+	assertions "github.com/aws/aws-cdk-go/awscdk/v2/assertions"
+	
+	"table"
+)
+
+func TestTableStack(t *testing.T){
+	// GIVEN
+	app := awscdk.NewApp(nil)
+
+	// WHEN
+	stack := table.TableStack(app, "myStack", nil)
+
+	// THEN
+	template := assertions.Template_FromStack(stack)
+
+	template.HasResourceProperties(aws.String("AWS::DynamoDB::Table"), map[string]interface{}{
+		"UpdateReplacePolicy": "Delete",
+	})
+}
