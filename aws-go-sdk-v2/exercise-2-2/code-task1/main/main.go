b23b8eca8f9f901a81fd31f5a6f7b48a5daa2e56
Gernot Glawe <gglawe@tecracer.de>
2023-05-09 09:04:40 +0200

    CodeDoc tags

--- aws-go-sdk-v2/exercise-2-2/code-task1/main/main.go
+++ aws-go-sdk-v2/exercise-2-2/code-task1/main/main.go
@@ -8,20 +8,27 @@ import (
 )
 
 func main() {
+	//begin arguments
 	argLength := len(os.Args[1:])
 	if argLength == 0 {
 		fmt.Printf("Please provide a stack name as an argument\n")
+		os.Exit(1)
 	} 
 	stackName := os.Args[1]
+	//end arguments
+	//begin call
 	resources, err := crlist.GetStatus(crlist.Client,&stackName)
 	if err != nil {
 		panic(err)
 	}
+	//end call
+
+	//begin display
 	fmt.Printf("%-32s %-32s \n", "Logical ID",  "Status")
 	fmt.Printf("%-32s %-32s\n", "----------", "-----------")
-	
 	for _, resource := range *resources {
 		fmt.Printf("%-32s %-32s\n", resource.LogicalID, resource.Status)
 	}
+	//end display
 
 }
---
+++ changelog.md
@@ -0,0 +1,10 @@
+# Changelog
+
+## [Todo]
+
+### Fixed
+
+- https://www.go-on-aws.com/getting_started/controlflow/control-flow-if/
+  s/         // Uncomment one on these three lines
+  one of
+
