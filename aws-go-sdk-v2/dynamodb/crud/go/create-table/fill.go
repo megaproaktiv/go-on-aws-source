d24f76e9c298cedee77c77f78df711ecf469c6a5
Gernot Glawe <gglawe@tecracer.de>
2023-05-20 15:37:19 +0200

    Start filling the table

---
+++ aws-go-sdk-v2/dynamodb/crud/go/create-table/fill.go
@@ -0,0 +1,56 @@
+package table
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"os"
+
+	"github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+)
+
+type BarJoke struct {
+	Name     string `json:"name" dynamodbav:"NAME"`
+	Rating   int    `json:"rating"`
+	Headline string `json:"headline"`
+	Content  string `json:"content"`
+}
+
+func FillTable(table *string) error{
+	// Read JSON file
+	fileBytes, err := os.ReadFile("items.json")
+	if err != nil {
+		fmt.Println("failed to read JSON file:", err)
+		return err
+	}
+
+	// Unmarshal JSON into a slice of items
+	var items []BarJoke
+	err = json.Unmarshal(fileBytes, &items)
+	if err != nil {
+		fmt.Println("failed to unmarshal JSON:", err)
+	}
+
+	// Put each item into DynamoDB
+	for _, record := range items {
+		av, err := attributevalue.MarshalMap(record)
+		if err != nil {
+			fmt.Println("failed to marshal item:", err)
+			return err
+		}
+
+		input := &dynamodb.PutItemInput{
+			TableName: table,
+			Item:      av,
+		}
+
+		_ ,err = Client.PutItem(context.TODO(), input)
+		if err != nil {
+			fmt.Println("failed to write item to DynamoDB:", err)
+			return err
+		}
+		
+	}
+	return nil
+}
--- aws-go-sdk-v2/dynamodb/crud/go/create-table/main/main.go
+++ aws-go-sdk-v2/dynamodb/crud/go/create-table/main/main.go
@@ -25,5 +25,12 @@ func main() {
 		os.Exit(2)
 	}
 	fmt.Printf("Table created \n")
+	fmt.Printf("Fill table \n")
+	err = table.FillTable(aws.String(tableName))
+	if err != nil {
+		fmt.Printf("Error filling table %v \n", err)
+		os.Exit(3)
+	}
+	fmt.Printf("Table filled \n")
 
 }
