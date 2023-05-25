e2e435cb30c9fb8c3fddc1cc471b6243a80839cd
Gernot Glawe <gglawe@tecracer.de>
2023-05-25 10:38:36 +0200

    codedoc

--- architectures/serverless/app/main/main.go
+++ architectures/serverless/app/main/main.go
@@ -1,16 +1,22 @@
 package main
 
+
 import (
 	"context"
 	"os"
 	"dsl"
+	//begin events
 	"github.com/aws/aws-lambda-go/events"
+	
+	//end events
 	"github.com/aws/aws-lambda-go/lambda"
 
 
 )
 // HandleRequest S3 Event
+//begin events
 func handler(ctx context.Context, s3Event events.S3Event) {
+//end events
 	// See https://github.com/aws/aws-lambda-go/tree/master/events
 	// Handle only one event
 	s3input := dsl.ExtractKey(s3Event);
