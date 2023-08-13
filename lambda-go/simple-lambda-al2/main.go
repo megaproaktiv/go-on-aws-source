68bfdf234f2e93d87d6ecef636384be354c6af9f
Gernot Glawe <gglawe@tecracer.de>
2023-08-13 20:04:36 +0200

    CodeDoc

--- lambda-go/simple-lambda-al2/main.go
+++ lambda-go/simple-lambda-al2/main.go
@@ -1,19 +1,22 @@
+// begin main
 package main
 
 import (
-        "fmt"
-        "context"
-        "github.com/aws/aws-lambda-go/lambda"
+	"context"
+	"fmt"
+	"github.com/aws/aws-lambda-go/lambda"
 )
 
 type MyEvent struct {
-        Name string `json:"name"`
+	Name string `json:"name"`
 }
 
 func HandleRequest(ctx context.Context, name MyEvent) (string, error) {
-        return fmt.Sprintf("Hiho %s!", name.Name ), nil
+	return fmt.Sprintf("Hiho %s!", name.Name), nil
 }
 
 func main() {
-        lambda.Start(HandleRequest)
+	lambda.Start(HandleRequest)
 }
+
+//end main
